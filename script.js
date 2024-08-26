07ad6962fb165f2d95f39f6a85ea551e48ee19eb
Kofoworol <abidemi.deleke@gmail.com>
2024-08-27 00:41:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let LanguageData = [
+  {
+    name: "English",
+    rate: 91,
+  },
+  {
+    name: "French",
+    rate: 45,
+  },
+  {
+    name: "Arabic",
+    rate: 25,
+  },
+  {
+    name: "Spanish",
+    rate: 24,
+  },
+  {
+    name: "Portuguese",
+    rate: 9,
+  },
+  {
+    name: "Russian",
+    rate: 9,
+  },
+  {
+    name: "Dutch",
+    rate: 8,
+  },
+  {
+    name: "German",
+    rate: 7,
+  },
+  {
+    name: "Chinese",
+    rate: 5,
+  },
+  {
+    name: "Serbian",
+    rate: 4,
+  },
+];
